4f7f3e541db813a99933a0d3994e2e7ee272979a
root <root@localhost.localdomain>
2018-03-16 20:26:34 +0800

    add server.py

---
+++ corn/server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import tornado.ioloop
+import tornado.web
+import json
+from pymongo import MongoClient
+import os
+import logging
+
+import tornado.options
+from tornado.options import define, options
+
+
+define("port", default=8999, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"\/*.*", IndexHandler),
+        ]
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            template_path=os.path.join(os.path.dirname(__file__), ""),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+class BaseHandler(tornado.web.RequestHandler):
+    pass
+class IndexHandler(BaseHandler):
+    def get(self):
+        self.render('index.html')
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    logging.info('Serving HTTP on 0.0.0.0 port %d ...' % options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
